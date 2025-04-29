6a83739f8e75efb162e6d0a642a360e6a0872b53
Richard Lay-Flurrie <rlayfl@essex.ac.uk>
2025-04-29 17:18:07 +0100

    Added database requirements

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Routing
 
 @app.route("/")
 def index():
@@ -33,6 +41,12 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+# Database Connection
+
+
+
+# Database Objects
 
 if __name__ == '__main__':
     app.run(debug = True)
